06792db8b6ccccbbf309df14cc2b6815e0c8cdd9
TOTH Benoit <47602460+Ahzenrei@users.noreply.github.com>
2023-02-23 23:13:28 +0100

    First window (for real)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    platform::run_return::EventLoopExtRunReturn,
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello world");
+    let window_builder = WindowBuilder::new(); 
+    let mut event_loop = EventLoop::new();
+    let _window = window_builder.build(&event_loop).expect("window");
+
+    let mut running = true;
+
+    while running {
+        event_loop.run_return(|event, _, control_flow| {
+            match &event {
+                Event::WindowEvent { event, .. } => match event {
+                    WindowEvent::CloseRequested => {
+                        *control_flow = ControlFlow::Exit;
+                        running = false;
+                    }
+                    _ => {}
+                },
+                _ => (),
+            }
+        });
+    }
 }
